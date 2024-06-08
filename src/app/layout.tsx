cd5d2ffc9875737c678ee87382c079fc4f53d2c7
loc-dev <nguyentheloc310101@gmail.com>
2024-06-08 20:01:45 +0700

    fix(jsvecor): fix dev dependencies for vector

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-"use client";
-import "jsvectormap/dist/css/jsvectormap.css";
-import "flatpickr/dist/flatpickr.min.css";
-import "@/css/satoshi.css";
-import "@/css/style.css";
-import React, { useEffect, useState } from "react";
-import Loader from "@/components/common/Loader";
+'use client';
+import 'jsvectormap/dist/css/jsvectormap.css';
+import 'flatpickr/dist/flatpickr.min.css';
+import '@/css/satoshi.css';
+import '@/css/style.css';
+import React, { useEffect, useState } from 'react';
+import Loader from '@/components/common/Loader';
 
 export default function RootLayout({
   children,
